93ac63b0a413ff9d313b3954e8cdd69c6da4c6f7
Abhishek Biswas <abhishekbiswas876@gmail.com>
2025-05-27 16:04:20 +0530

    Test commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Navbar from "./components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import useFetch from "./hooks/useFetch";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#34C94B'
+    }
+  }
+})
+
+function App() {
+
+  const { response: genres } = useFetch('/genres')
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar />
+        <Outlet context={{genres: genres}} />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
